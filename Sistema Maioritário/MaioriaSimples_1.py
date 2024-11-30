551ec28e9fb83529b2ca7b1aa7d605c5a6c0e2c7
JoseGomes <goishelder@hotmail.com>
2024-11-30 13:52:12 +0000

    Update MaioriaSimples_1.py

--- Sistema Maioritário/MaioriaSimples_1.py
+++ Sistema Maioritário/MaioriaSimples_1.py
@@ -1,4 +1,18 @@
-# Versão 1
-lista_A = 123
-lista_B = 456
-lista_C = 567
+## Versão simples de um sistema maioritário simples.
+## Limitado a três listas e não verifica se há empate entre as listas mais votadas
+
+# As listas e os respetivos votos
+lista_A = 123 #votos
+lista_B = 456 #votos
+lista_C = 567 #votos
+
+if(lista_A > lista_B):
+  if(lista_A > lista_C):
+    print("A lista vencedora é a lista A")
+  else:
+    print("A lista vencedora é a lista C")
+else:
+  if(lista_B > lista_C):
+    print("A lista vencedora é a lista B")
+  else:
+    print("A lista vencedora é a lista C")
